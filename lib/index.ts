bfd4763cecf7a5e627834b9f238163e28c2a8dce
Justin D'Ottavio <justin@bytrellus.com>
2025-05-19 16:57:48 -0400

    add resource exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,16 @@
 import Onfleet from "./onfleet.js";
+export * from "./resources/administrators.js";
+export * from "./resources/containers.js";
+export * from "./resources/customFields.js";
+export * from "./resources/destinations.js";
+export * from "./resources/hubs.js";
+export * from "./resources/organization.js";
+export * from "./resources/recipients.js";
+export * from "./resources/routeOptimizations.js";
+export * from "./resources/routePlan.js";
+export * from "./resources/tasks.js";
+export * from "./resources/teams.js";
+export * from "./resources/webhooks.js";
+export * from "./resources/workers.js";
 
 export default Onfleet;
